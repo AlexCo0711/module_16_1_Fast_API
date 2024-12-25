68f0369955fa5f29901075f2b09a9011a5d0bf59
Александр <alexco@list.ru>
2024-12-25 23:15:14 +0300

    Add files via upload
    Домашнее задание по теме "Основы Fast Api и маршрутизация"

---
+++ Module_16_1.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Создано приложение(объект) FastAPI
+app = FastAPI()
+
+# Создан декоратор маршрута к главной странице - "/"
+@app.get("/")
+async def get_p_main() -> dict:
+    return {'message': 'Главная страница'}
+
+# Создан декоратор маршрута к странице администратора
+@app.get("/user/admin")
+async def get_p_admin() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+# Создан декоратор маршрута к страницам пользователей используя параметр в пути
+@app.get("/user/{user_id}")
+async def get_p_user_id(user_id: int) -> dict:
+    return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+# Создан декоратор маршрута к страницам пользователей передавая данные в адресной строке
+@app.get("/user")
+async def get_p_data_user(username: str, age: int) -> dict:
+    return {'message':
+            f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
